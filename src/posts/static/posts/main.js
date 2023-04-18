214ae143fd8ca5dfa6cf65f95e20a6bad6588d0e
Maísa Wolff Resplande <maisawres@gmail.com>
2023-04-18 15:50:13 -0400

    Get CSRF from Django documentation

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -5,6 +5,24 @@ const spinnerBox = document.getElementById('spinner-box'); // get spinner div
 const loadBtn = document.getElementById('load-btn');
 const endBox = document.getElementById('end-box');
 
+// Get CSRF token
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
 let visible = 3;    // initial number of posts to display
 
 // getData will be triggered by a button clicked
@@ -26,8 +44,7 @@ const getData = () => {
                         <div class="card mb-2">
                             <div class="card-body">
                                 <h5 class="card-title">${element.title}</h5>
-                                <p class="card-text">${element.body}</p>
-                                
+                                <p class="card-text">${element.body}</p>      
                             </div>
                             <div class="card-footer">
                                 <div class="row">
@@ -35,7 +52,10 @@ const getData = () => {
                                         <a href="#" class="btn btn-primary">Details</a>
                                     </div>
                                     <div class="col-2">
-                                        <a href="#" class="btn btn-primary">${element.liked ? `Unlike (${element.count})`: `Like (${element.count})`}</a>
+                                        <form class="like-unlike-forms" data-form-id="${element.id}"> <!-- get element id to handle the likes of each post -->  
+                                            
+                                            <button href="#" class="btn btn-primary">${element.liked ? `Unlike (${element.count})`: `Like (${element.count})`}</button> <!-- show unlike if the post already liked -->
+                                        </form>
                                     </div>
                                 </div>
                             </div>
