d5cfe94f69dc85ad7d18495d2874651ceb4e8783
Maísa Wolff Resplande <maisawres@gmail.com>
2023-04-18 00:53:28 -0400

    Finishes set up of signals to create a profile when the user is created

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
-def post_save_create_profile(sender, instance, created, *args, **kwargs)
+def post_save_create_profile(sender, instance, created, *args, **kwargs):
     print(sender)
     print(instance)
     print(created)
