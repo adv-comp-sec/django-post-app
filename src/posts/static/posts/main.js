70b74a1206fbd917b84ae7c473040367af83d233
Maísa Wolff Resplande <maisawres@gmail.com>
2023-04-18 14:44:22 -0400

    Use ajax call to display the data with the posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log('hello world');
 
 const helloWorldBox = document.getElementById('hello-world');
+const postsBox = document.getElementById('posts-box');  // div to display the posts
 
 // ajax call with DOM manipulation
 $.ajax({
@@ -12,6 +13,26 @@ $.ajax({
     },
     error: function(error) {
         console.log('error', error);
+    },
+})
+
+// ajax call to get the data
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) {
+        console.log(response);
+        const data = response.data;     // data is an array from views.py
+        console.log(data);
+        // loop the data array and display each post within the div
+        data.forEach(element => {
+            postsBox.innerHTML += `
+                ${element.title} - <b>${element.body}</b><br>
+            `
+        });
+    },
+    error: function(error) {
+        console.log(error);
     },
 
 })
--- src/posts/views.py
+++ src/posts/views.py
@@ -8,5 +8,19 @@ def post_list_and_create(request):
     qs = Post.objects.all()         # get all posts from database
     return render(request, 'posts/main.html', {'qs':qs})  # return the request to template
 
+# return a JSON response
+def load_post_data_view(request):
+    qs = Post.objects.all()
+    data = []                   # create a list to save the values of each object
+    for obj in qs:
+        item = {
+            'id': obj.id,
+            'title': obj.title,
+            'body': obj.body,
+            'author': obj.author.user.username
+        }
+        data.append(item)
+    return JsonResponse({'data': data})
+
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
