216cbd14a40110d717a66e3114c3f7898ba08884
Maísa Wolff Resplande <maisawres@gmail.com>
2023-04-18 14:56:33 -0400

    Add spinner before loading posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log('hello world');
 
 const helloWorldBox = document.getElementById('hello-world');
 const postsBox = document.getElementById('posts-box');  // div to display the posts
+const spinnerBox = document.getElementById('spinner-box'); // get spinner div
 
 // ajax call with DOM manipulation
 $.ajax({
@@ -23,13 +24,17 @@ $.ajax({
     success: function(response) {
         console.log(response);
         const data = response.data;     // data is an array from views.py
-        console.log(data);
-        // loop the data array and display each post within the div
-        data.forEach(element => {
-            postsBox.innerHTML += `
-                ${element.title} - <b>${element.body}</b><br>
+        // timeout to display the spinner before the data is load
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible');
+            console.log(data);
+            // loop the data array and display each post within the div
+            data.forEach(element => {
+                postsBox.innerHTML += `
+                    ${element.title} - <b>${element.body}</b><br>
             `
         });
+        }, 100)      
     },
     error: function(error) {
         console.log(error);
