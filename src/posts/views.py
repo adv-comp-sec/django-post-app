f50dd677d5716f98681c2b6b18da9be2fccd06c2
Maísa Wolff Resplande <maisawres@gmail.com>
2023-04-18 13:05:29 -0400

    Include method to render the posts fetched from database.

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list_and_create(request):
+    qs = Post.objects.all()         # get all posts from database
+    return render(request, '', {'qs':qs})   # return the request to template
