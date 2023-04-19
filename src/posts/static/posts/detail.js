bf120f3dd799f4698436fb64c9522289ca6bedd5
Maísa Wolff Resplande <maisawres@gmail.com>
2023-04-18 20:02:50 -0400

    Check if the author is the user logged in

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -3,6 +3,8 @@ console.log('hello world detail')
 
 // variables
 const backBtn = document.getElementById('back-btn');
+const updateBtn = document.getElementById('update-btn');
+const deleteBtn = document.getElementById('delete-btn');
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
 
@@ -11,12 +13,22 @@ backBtn.addEventListener('click', () => {
     history.back()
 })
 
-//
+// ajax call 
 $.ajax({
     type: 'GET',
     url: url,
     success: function(response){
-        console.log(response)
+        console.log(response);
+        const data = response.data;
+
+        // check if the author is the user logged in
+        if (data.logged_in !== data.author) {
+
+        } else {
+            updateBtn.classList.remove('not-visible');
+            deleteBtn.classList.remove('not-visible');
+        }
+
         spinnerBox.classList.add('not-visible');
     },
     error: function(error){
